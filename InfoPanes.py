0d136b4c9d0872c96f551410c28c6b90be9fc2dd
Myles Clark <blokart11@gmail.com>
2013-10-28 14:24:19 +1030

    Fixed further maths errors

--- InfoPanes.py
+++ InfoPanes.py
@@ -2,7 +2,7 @@ import quick2wire.i2c as i2c
 import curses
 
 class VReader:
-  def __init__(self, adc, ch, Lcal):
+  def __init__(self, adc, ch, Lcal, conv = 1):
     self.adc = adc
     self.ch = ch
 
@@ -10,6 +10,7 @@ class VReader:
     self.varDiv  = 16
     self.varMult = (2.4705882/self.varDiv)/1000
     self.Lcal = Lcal
+    self.conv = conv
 
   def getadcreading(self, bus):
     #Change the ADC chip channel
@@ -25,23 +26,22 @@ class VReader:
     if (h > 128):
       v = ~(0x020000 - v)
 
-    return ((self.varMult * v) - self.Lcal[1]) / self.Lcal[0]
+    return ((self.varMult * v * conv) - self.Lcal[1]) / self.Lcal[0]
 
 class DiodePane(VReader):
   def __init__(self, idNum, x, y, adc, ch, Lcal = (1, 0)):
-    super(DiodePane, self).__init__(adc, ch, Lcal)
+    super(DiodePane, self).__init__(adc, ch, Lcal, 10)
     self.posX = x
     self.posY = y
     self.h = 1
     self.w = 30
-    self.conv = 10
     self.idNum = idNum
 
     self.win = curses.newwin(self.h, self.w, self.posY, self.posX)
 
   def getTemperature(self, bus):
     v = self.getadcreading(bus)
-    temp = v * self.conv
+    temp = v
 
     return temp
 
